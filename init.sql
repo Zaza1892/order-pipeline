a73931acfd7ad0c879cbd58d5aa34c2eb508d094
Zaza1892 <90157965+Zaza1892@users.noreply.github.com>
2026-07-27 10:17:58 +0200

    Create init.sql

---
+++ init.sql
@@ -0,0 +1,10 @@
+create table order_lookups (
+    id serial primary key,
+    message_id text unique not null,
+    order_id int not null,
+    customer_id int not null,
+    adress text ,
+    products JSONB,
+    event_timestamp TIMESTAMPZ,
+    raw_payload JSONB
+)
